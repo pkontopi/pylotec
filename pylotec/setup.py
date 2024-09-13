5ef370e1c0705dc717b077b164c022e6d5c5fc5a
pkontopi <165373833+pkontopi@users.noreply.github.com>
2024-09-13 15:19:19 +0200

    Update setup.py

--- pylotec/setup.py
+++ pylotec/setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='pylotec',
     version='1.0',
-    packages=find_packages(include=['pylotec', 'pylotec.*']),
+    author = "Simon Linke, Patrick Kontopidis",
+    package_dir={"": "pylotec"},
+    include_package_data=True,
+    packages=["pylotec"],
+    package_data = {"pylotec": ["./pylotec/*.py"], "pylotec": ["./Functions/*.py"], "pylotec": ["./old/*.ipynb"]},
     install_requires=['numpy', 'matplotlib', 'pillow', 'tqdm', 'scipy', 'pandas', 'plotly'],
 )
